af425fc8c90bb0af6df19f622239f0e19cf9ee89
Gradlon von Känel <gradlon@integral-systems.ch>
2018-11-24 15:19:12 +0100

    Added Gatsby

---
+++ Gatsyby/tutorial-part-three/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Gradlon\\Desktop\\tutorial-part-three\\.cache\\dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Gradlon\\Desktop\\tutorial-part-three\\src\\pages\\index.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\Gradlon\\Desktop\\tutorial-part-three\\src\\pages\\about.js")),
+  "component---src-pages-contact-js": preferDefault(require("C:\\Users\\Gradlon\\Desktop\\tutorial-part-three\\src\\pages\\contact.js"))
+}
+
